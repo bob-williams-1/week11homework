162bd07dbd0fb716539bf80278547a5ea20d981c
bob-williams-1 <bob.williams.dev@gmail.com>
2018-11-06 11:05:32 -0500

    letter.js

--- letter.js
+++ letter.js
@@ -1,6 +1,4 @@
-// @author: Thomas Thompson
-// @github: tomtom28
-// @comment: Homework 11 - Hangman Node Game
+
 
 // Constructor File
 // letter.js should control whether or not a letter appears as a "_" or as itself on-screen.
